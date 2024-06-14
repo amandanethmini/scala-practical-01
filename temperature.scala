435ca06a6fe87bc7f6175344eded8aba7b6efc04
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-06-14 10:47:10 -0700

    Add files via upload

---
+++ temperature.scala
@@ -0,0 +1,9 @@
+object temperature {
+  def temp(t: Double) = t * 1.8000 + 32.0
+
+  def main(args: Array[String]): Unit = {
+    var t = 35
+    var temprature = temp(t)
+    println("Temperature is in Fahrenheit " + temprature)
+  }
+}

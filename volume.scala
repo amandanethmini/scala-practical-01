de710eacd07abbd6232220ec5b0077aa762f45c7
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-06-14 10:48:27 -0700

    Add files via upload

---
+++ volume.scala
@@ -0,0 +1,9 @@
+object volume {
+  def volumeOfSphere(r: Double) = math.Pi * r * r * r * 4 / 3
+
+  def main(args: Array[String]): Unit = {
+    var r = 5
+    var volume = volumeOfSphere(r)
+    println("volume of a sphere is " + volume)
+  }
+}

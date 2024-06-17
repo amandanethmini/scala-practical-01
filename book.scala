b694a60d1521901b6d776c72eb926e330eb2d4f1
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-06-17 09:46:09 -0700

    Update book.scala

--- book.scala
+++ book.scala
@@ -2,7 +2,7 @@ object book {
   def bookCoverPrice(n: Int) = n * 24.95
   def discount(n: Int): Double = bookCoverPrice(n) * 0.4
   def shippingCost(n: Int): Double =
-    if (n <= 50) n * 3 else (n * 3) + (n - 50) * 75
+    if (n <= 50) n * 3 else (n * 3) + (n - 50) * 0.75
   def total(n: Int): Double = bookCoverPrice(n) - discount(n) + shippingCost(n)
 
   def main(args: Array[String]): Unit = {
